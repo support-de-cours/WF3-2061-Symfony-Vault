7a38ab527e559ff616833c50a83e6190fa905ad0
Arnaud BODEL <contact@osw3.net>
2023-05-31 10:52:29 +0200

    -

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -11,8 +11,6 @@ class ProfileController extends AbstractController
     #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
-        ]);
+        return $this->render('profile/index.html.twig', []);
     }
 }
